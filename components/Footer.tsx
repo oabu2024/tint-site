50d7fe5b4fccf7b19a44e81f6504328566ccc6de
Abdul <abdulawes213@gmail.com>
2026-08-26 23:32:22 -0500

    Link FB icon to Tint Bros Facebook page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -61,7 +61,9 @@ export default function Footer() {
               {["IG", "FB", "TW", "YT"].map((s) => (
                 <a
                   key={s}
-                  href="#"
+                  href={s === "FB" ? "https://www.facebook.com/profile.php?id=61591152245842" : "#"}
+                  target={s === "FB" ? "_blank" : undefined}
+                  rel={s === "FB" ? "noopener noreferrer" : undefined}
                   style={{
                     width: "38px",
                     height: "38px",
